7521e31575510504b21cc8cb73ab4ba7a1684351
Ranjan Dissanayake <89340998+dissanayakerbn@users.noreply.github.com>
2023-03-27 08:01:16 +0530

    Add files via upload

---
+++ public/scripts/tre.js
@@ -0,0 +1,8 @@
+console.log("js file linked");
+
+function naviagateToForm(){
+    window.location.href="form.html";
+}
+function naviagateToThankYou(){
+    window.location.href="thankyou.html";
+}
